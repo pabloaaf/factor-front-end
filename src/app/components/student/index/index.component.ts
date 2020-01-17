17e30f3dcf16751074db21db4700be38e9ca3c29
pabloaaf <palvaf02@estudiantes.unileon.es>
2020-01-17 13:39:57 -0600

    student show courses info, minus videos

--- src/app/components/student/index/index.component.ts
+++ src/app/components/student/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-index',
@@ -7,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StdIndexComponent implements OnInit {
   private userInfo:any;
-  constructor() { }
+  private coursesInfo:any;
+  private uri:string;
+
+  constructor(private http: HttpClient) {
+    this.uri = 'http://192.168.1.125:3000'; //localhost
+  }
 
   ngOnInit() {
     let token = sessionStorage.getItem('token');
     this.userInfo = JSON.parse(atob(token.split('.')[1]));
+    this.getUserInfo();
+  }
+
+  getUserInfo(){
+    this.http.get(`${this.uri}/users/` + this.userInfo._id).subscribe((data: any) => {
+      this.userInfo = data;
+      console.log(this.userInfo);
+      this.getCoursesInfo();
+    }, (error: any) => {console.log(error);});
+  }
+
+  getCoursesInfo(){
+    this.http.post(`${this.uri}/courses/id`,{id:this.userInfo.courses}).subscribe((data: any) => {
+      this.coursesInfo = data;
+      console.log(this.coursesInfo);
+      let professorsID = [];
+      for (let i = 0; i < this.coursesInfo.length; i++) {
+        professorsID[i] = Number(this.coursesInfo[i].professor);
+      }
+      this.getProfessorsInfo(professorsID);
+    }, (error: any) => {console.log(error);});
+  }
+
+  getProfessorsInfo(PIDs) {
+    this.http.post(`${this.uri}/users/id`,{id:PIDs}).subscribe((data: any) => {
+      for (let i = 0; i < data.length; i++) {
+        this.coursesInfo[i].professorInfo = data[i];
+      }
+      console.log(this.coursesInfo);
+    }, (error: any) => {console.log(error);});
   }
 }
