cbf38e31b9c6db0a249598faf26596cc41151fbd
pabloaaf <palvaf02@estudiantes.unileon.es>
2020-01-17 13:39:15 -0600

    professor upload videos partial

--- src/app/components/professor/index/index.component.ts
+++ src/app/components/professor/index/index.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-index',
@@ -7,15 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfIndexComponent implements OnInit {
   private userInfo:any;
-  private courses:any;
-  constructor() { }
+  uploadVideoForm: FormGroup;
+  submitted = false;
+  private url:SafeResourceUrl;
+
+
+  constructor(private formBuilder: FormBuilder, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     let token = sessionStorage.getItem('token');
     this.userInfo = JSON.parse(atob(token.split('.')[1]));
+    this.uploadVideoForm = this.formBuilder.group({
+      course: ['', Validators.required],
+      video: ['', Validators.required]
+    });
   }
 
-  public saveVideo(c,f){
+  get f() { return this.uploadVideoForm.controls; }
 
+  public saveVideo(){
+    this.submitted = true;
+
+    // stop the process here if form is invalid
+    if (this.uploadVideoForm.invalid) {
+      return;
+    }
+
+    console.log('SUCCESS!!');
+    console.log(this.uploadVideoForm.get('course').value);
+  }
+
+  public onSelectFile(event) {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = (event) => {
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl(<string>(<FileReader>event.target).result);
+      }
+    }
   }
 }
