dc435076d985c6651163251b894d0de1b109a939
pabloaaf <palvaf02@estudiantes.unileon.es>
2020-02-04 23:21:03 -0600

    detach footer from component

--- src/app/components/navbar/footer/footer.component.ts
+++ src/app/components/navbar/footer/footer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
+  selector: '[app-footer]',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
