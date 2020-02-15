c9fe5258ee539b86a6f64a8c4f5b21697b861caa
pabloaaf <palvaf02@estudiantes.unileon.es>
2020-02-15 01:01:26 -0600

    show transcripts on view

--- src/app/components/video/show-video/show-video.component.ts
+++ src/app/components/video/show-video/show-video.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ViewChild,OnInit,ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 //import { HttpClient } from '@angular/common/http';
 import {GlobalsComponent, User, Video} from "../../../globals/globals.component";
@@ -14,6 +14,9 @@ export class ShowVideoComponent implements OnInit {
   //private videoID: string;
   userInfo: User;
   videoInfo: Video;
+  videoTranscribes: any;
+  @ViewChild('vid', {static: false}) video: ElementRef;
+
   constructor(private _httpService: HTTPService, private route: ActivatedRoute) {
     this.uri = GlobalsComponent.api+GlobalsComponent.version; //localhost
     let token = JSON.parse(atob(sessionStorage.getItem('token').split('.')[1]));
@@ -21,52 +24,21 @@ export class ShowVideoComponent implements OnInit {
       this.userInfo = <User>us;
       this._httpService.getVideoIdInfo(this.route.snapshot.paramMap.get('id')).subscribe(video => {
         this.videoInfo = <Video>video;
-      });
-    });
-    // this.videoInfo = new Video();
-  } //private route: ActivatedRoute
-
-  ngOnInit() {
-    /*let videoID = this.route.snapshot.paramMap.get('id');
-    let token = sessionStorage.getItem('token');
-    this.userInfo = JSON.parse(atob(token.split('.')[1]));
-    this.getUserInfo();
-
-      this.userInfo = <User>us;
-      this._httpService.getCoursesInfo(this.userInfo.coursesID).subscribe(courses => {
-        this.coursesInfo = <Course[]>courses;
-        let professorsID = [];
-        for (let i = 0; i < this.coursesInfo.length; i++) {
-          professorsID[i] = Number(this.coursesInfo[i].professorID);
-        }
-        this._httpService.getProfessorsInfo(professorsID).subscribe(prof => {
-          this.professorInfo = <User[]>prof;
+        console.log(this.video.nativeElement.currentTime);
+        this.video.nativeElement.currentTime = 10;
+        this._httpService.getVideoTranscribes(this.route.snapshot.paramMap.get('id')).subscribe(transcribes => {
+          this.videoTranscribes = transcribes;
         });
       });
-      this._httpService.getVideosInfo(this.userInfo.coursesID).subscribe(videos => {
-        this.videosInfo = <Video[]>videos;
-      });
-*/
-    /*if(this.route.snapshot.url[0].path == 'prof'){
-      console.log('professor');
-    } else {
-      console.log('student');
-    }*/
-
+    });
   }
 
-/*  getUserInfo(){
-    this.http.get(`${this.uri}users/` + this.userInfo._id).subscribe((data: any) => {
-      this.userInfo = data;
-      console.log(this.userInfo);
-      this.getVideoInfo();
-    }, (error: any) => {console.log(error);});
-  }
+  ngOnInit() {}
 
-  getVideoInfo(){
-    this.http.post(`${this.uri}videos/id`,{id:this.videoID}).subscribe((data: any) => {
-      this.videoInfo = data;
-      console.log(this.videoInfo);
-    }, (error: any) => {console.log(error);});
-  }*/
+  public setVideoTime(time:string) {
+    this.video.nativeElement.currentTime = Number.parseInt(time);
+    console.log(this.video.nativeElement.currentTime);
+    this.video.nativeElement.scrollIntoView({behavior:"smooth"});
+
+  }
 }
