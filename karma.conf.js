d369bbb6bc7ca36888b69c137d8e21677b3a5230
pabloaaf <palvaf02@estudiantes.unileon.es>
2020-04-02 20:27:32 -0600

    browser

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-const ChromiumRevision = require('puppeteer/package.json').puppeteer.chromium_revision
-const Downloader = require('puppeteer/utils/ChromiumDownloader')
-const revisionInfo = Downloader.revisionInfo(Downloader.currentPlatform(), ChromiumRevision)
-
-process.env.CHROME_BIN = revisionInfo.executablePath
-
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+console.log(process.env.CHROME_BIN)
 module.exports = function (config) {
   config.set({
     basePath: '',
