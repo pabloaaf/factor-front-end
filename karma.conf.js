f1bb412c390f6cf3185bd32b9154e49473363dc9
pabloaaf <palvaf02@estudiantes.unileon.es>
2020-04-02 18:55:34 -0600

    browser change

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
+  process.env.CHROME_BIN = require('puppeteer').executablePath();
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -32,11 +32,7 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--headless', 
-          '--remote-debugging-port=9222',
-          '--no-sandbox', 
-          '--proxy-server=\'direct://\'',
-          '--proxy-bypass-list=*']
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-web-security']
       }
     }
   });
